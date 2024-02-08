c3ce5219f6d6d7616b8f3ae40b054adbfa6bd51b
diega <diegochiola@gmail.com>
2024-02-08 13:01:09 +0100

    Creacion de conecciones en archivo config

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+//constantes para configurar la conexion
+const DB_HOST = "localhost";
+const DB_NAME = "db_toDo"; //nombre de la base de datos que creamos
+const DB_USER = "root";
+const DB_PASSWORD = ""; //se deja vacia si no hay una contraseña especifica
+const DB_CHARSET = "utf8";
+
+const BASE_URL = "http://localhost/Bootcamp_php/sprint3_Developer/"; //direccion raiz del prooyecto
+?>
