c6110d6306bb6cb747c0ec632f799d19a06d20bf
diega <diegochiola@gmail.com>
2024-02-12 21:04:54 +0100

    Creacion de clase Task y declaracion de funciones CRUD sin desarrollar

---
+++ app/models/task.php
@@ -0,0 +1,26 @@
+<?php
+class Task{
+    //Atributos
+    public int $task_id;
+    public string $task_name;
+    public string $created_at;
+    public string $finished_at;
+    public TaskStatus $status;
+
+    //metodo Constructor
+    public function __construct(){}
+    //Metodo Save Task
+    public function saveTask(){}
+    //Metodo Upload/Edit Task
+    public function uploadTask(){}
+    //Metodo Delete Task
+    public function deleteTask(){}
+
+
+
+
+
+
+}
+
+
