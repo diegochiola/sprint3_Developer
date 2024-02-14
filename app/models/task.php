2a164ecf482fb4ae5e161fcfbde027c01913a7e8
diega <diegochiola@gmail.com>
2024-02-14 10:39:01 +0100

    incluir metodos proporcionados en lib/base/model.php

--- app/models/task.php
+++ app/models/task.php
@@ -1,16 +1,27 @@
 <?php
-class Task{
+requiere_once('lib/base/model.php'); 
+//llamamos al archivo model.php dentro de base para implementar metodos de este archivo
+
+class Task extends Model{
+   
     //Atributos
-    public int $task_id;
-    public string $task_name;
-    public string $created_at;
-    public string $finished_at;
-    public TaskStatus $status;
-    public string $created_by;
+    private $dataFile = 'data/tasks.json'; //donde ubicaremos el archivo json tasks
 
     //metodo Constructor
-    public function __construct(){}
+    public function __construct(){} //lo dejamos vacio porque utilizaremos la conexion al archivo json
     //Metodo Save Task
+    
+    //Metodo para obtener una tarea a travez de su id
+    public function fetchOne($id) {
+        $tasks = $this->loadData(); //Se carga el archivo JSON
+        foreach ($tasks as $task) { //Se recorren todas las tareas, tarea por tarea
+            if ($task['ID'] == $id) { //Si se encuentra una tarea con el ID proporcionado
+                return $task; // Se devuelve la tarea encontrada
+            }
+        }
+        return null; //En caso contrario, se devuelve null
+    }
+    
     public function saveTask(){}
     //Metodo Upload/Edit Task
     public function uploadTask(){}
