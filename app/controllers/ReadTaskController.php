83bd50bb60ff7e1f67b73120b68592de5755d18f
diega <diegochiola@gmail.com>
2024-02-14 23:12:25 +0100

    conectar con la vista tasks.php

--- app/controllers/ReadTaskController.php
+++ app/controllers/ReadTaskController.php
@@ -1,12 +1,17 @@
 <?php
 require_once('app/models/task.php');
+require_once('lib/base/Controller.php');
 
-class ReadTaskController{
+class ReadTaskController extends Controller{
   
 
     public function readTask(){
+        $this->init();
         $taskModel = new Task(); //instanciar la clase task
-        $taskModel->loadData(); //llamar al metodo create task de la clase task
+        $arrayTasks = $taskModel->loadData(); //cargar tareas
+        
+        $this->view->tasks = $arrayTasks; //se le designa a una variable d ela vista
+        $this->view->render('task/tasks.php'); //renderizamos la vista
     }
 }
 ?>
