6521d9b83d3cc908c10e5d09ff6f919c6878ca4d
diega <diegochiola@gmail.com>
2024-02-14 22:35:42 +0100

    incorporar metodos de lib/base/controller.php

--- app/controllers/UpdateTaskController.php
+++ app/controllers/UpdateTaskController.php
@@ -1,12 +1,16 @@
 <?php
 require_once('app/models/task.php');
+require_once('lib/base/Controller.php');
 
-class UpdateTaskController{
+class UpdateTaskController extends Controller{
   
 
-    public function updateTask($dataTask){
+    public function updateTask(){
+        $this->init();
+        $taskId= $this->_getParam('taskId');//id de la tarea a actualizar
+        
         $taskModel = new Task(); //instanciar la clase task
-        return $taskModel->fetchOne($dataTask); //llamar al metodo delete task de la clase task
+        $task = $taskModel->fetchOne($taskId); //actualizar la tarea
     }
 }
 ?>
