a9683194f0aa8221260dee0199a4b83a6e3d80be
diega <diegochiola@gmail.com>
2024-02-14 18:42:31 +0100

    edicion de  correccion de metodos en archivo

--- app/models/task.php
+++ app/models/task.php
@@ -1,19 +1,29 @@
 <?php
-requiere_once('lib/base/model.php'); 
+require_once('lib/base/model.php'); 
 //llamamos al archivo model.php dentro de base para implementar metodos de este archivo
 
 class Task extends Model{
-   
+    
     //Atributos
     private $dataFile = 'data/tasks.json'; //donde ubicaremos el archivo json tasks
 
     //metodo Constructor
-    public function __construct(){} //lo dejamos vacio porque utilizaremos la conexion al archivo json
+    public function __construct(){
+        parent::__construct();
+    } //lo dejamos vacio porque utilizaremos la conexion al archivo json
     
+    //metodo crear tarea
+    public function createTask($newTask){
+        //primero convertir json en array (load data)
+        $arrayTasks= $this->loadData();
+        $arrayTasks[]= $newTask; //agrego la nueva tarea
+        $this->saveData($arrayTasks);       
+    }
+
     //Metodo para obtener una tarea a traves de su id
     public function fetchOne($id) {
-        $tasks = $this->loadData(); //Se carga el archivo JSON
-        foreach ($tasks as $task) { //Se recorren todas las tareas, tarea por tarea
+        $arrayTasks = $this->loadData(); //Se carga el archivo JSON
+        foreach ($arrayTasks as $task) { //Se recorren todas las tareas, tarea por tarea
             if ($task['ID'] == $id) { //Si se encuentra una tarea con el ID proporcionado
                 return $task; // Se devuelve la tarea encontrada
             }
@@ -22,25 +32,27 @@ class Task extends Model{
     }
     //Metodo SaveTask
     public function save($task){
-        $tasks = $this->loadData(); //cargamos el archivo json primero
-        $tasks[]=$task; //agregamos la nueva task al array tasks
-        $this->saveData($tasks);// Se guarda el arreglo actualizado de tareas en el archivo JSON
+        $arrayTasks = $this->loadData(); //cargamos el archivo json primero
+        $arrayTasks[]=$task; //agregamos la nueva task al array tasks
+        $this->saveData($arrayTasks);// Se guarda el arreglo actualizado de tareas en el archivo JSON
 
     }
    
     //Metodo Delete Task por su id
     public function delete($id){
-        $tasks = $this->loadData();
-        foreach($tasks as $key => $task){
+        $arrayTasks = $this->loadData();
+        foreach($arrayTasks as $key => $task){
             if($task['ID']== $id){
                unset($tasks[$key]); //se elimina mediante unset del arreglo tasks
-               $this->saveData($tasks);
+               $arrayTasks = array_values($arrayTasks); // array values reorganiza indice
+               $this->saveData($arrayTasks);
                return; 
             }
         }
     }
    
     //Metodos para codificar y decodificar el archivo json
+   
     //metodo para cargar la data al archivo json
     private function loadData() { 
         $data = file_get_contents($this->dataFile); //leo el archivo JSON
