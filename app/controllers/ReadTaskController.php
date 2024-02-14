e1e9ce9b00ff126f6b88bc9fd66b30a04875e888
diega <diegochiola@gmail.com>
2024-02-14 23:22:30 +0100

    correccion de vista a renderizar: ['arrayTasks' => ]

--- app/controllers/ReadTaskController.php
+++ app/controllers/ReadTaskController.php
@@ -10,8 +10,8 @@ class ReadTaskController extends Controller{
         $taskModel = new Task(); //instanciar la clase task
         $arrayTasks = $taskModel->loadData(); //cargar tareas
         
-        $this->view->tasks = $arrayTasks; //se le designa a una variable d ela vista
-        $this->view->render('task/tasks.php'); //renderizamos la vista
+       //$this->view->tasks = $arrayTasks; //se le designa a una variable d ela vista
+       $this->view->render('task/tasks.php', ['arrayTasks' => $arrayTasks]); //renderizamos la vista
     }
 }
 ?>
